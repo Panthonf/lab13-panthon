2c6459574ca1620e6d54280d71505da519f9100e
Panthonf <panthon_kansap@cmu.ac.th>
2024-01-02 14:46:34 +0700

    Implement statistical calculations and image updating

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -17,3 +17,21 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[]){
+    double sum = 0,sum2 = 0,sum3 = 0,sum4 = 0,max = A[0],min = A[0];
+    for(int i = 0;i < N;i++){
+        sum += A[i];
+        sum2 += pow(A[i],2);
+        sum3 += 1/A[i];
+        sum4 += log10(A[i]);
+        if(A[i] > max) max = A[i];
+        if(A[i] < min) min = A[i];
+    }
+    B[0] = sum/N;
+    B[1] = sqrt((sum2/N)-pow(B[0],2));
+    B[2] = pow(10,sum4/N);
+    B[3] = N/sum3;
+    B[4] = max;
+    B[5] = min;
+}
